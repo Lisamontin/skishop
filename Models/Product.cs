d99188751c6b0feb7d2828f94d20dab6b3793978
Lisamontin <42090473+Lisamontin@users.noreply.github.com>
2021-01-12 12:43:05 +0100

    add product model

---
+++ Models/Product.cs
@@ -0,0 +1,10 @@
+public class Product
+{
+  public int Id { get; set; }
+  public string ProductName { get; set; }
+  public string Description { get; set; }
+  public int Price { get; set; }
+  public string ImgUrl { get; set; }
+  public int Size { get; set; }
+  public string Color { get; set; }
+}
