aa9a498d3baee0223d5b4ad8c6a4e1b15825e4b2
Lisamontin <42090473+Lisamontin@users.noreply.github.com>
2021-01-18 10:16:09 +0100

    add productorder DTO

---
+++ Models/DTOs/ProductOrderDTO.cs
@@ -0,0 +1,9 @@
+public class ProductOrderDTO
+{
+  public int Id { get; set; }
+  public int ProductId { get; set; }
+  public int OrderId { get; set; }
+  public int Quantity { get; set; }
+  public int TotalPrice { get; set; }
+
+}
