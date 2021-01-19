20b2f015d52bedef407a5b9cb81620acab0bc8a7
Lisamontin <42090473+Lisamontin@users.noreply.github.com>
2021-01-19 09:50:39 +0100

    add put and delete

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
@@ -7,7 +8,7 @@ using Microsoft.EntityFrameworkCore;
 namespace Products.Controllers
 {
   [ApiController]
-  [Route("[controller]")]
+  [Route("api/[controller]")]
   public class ProductsController : ControllerBase
   {
     private readonly SkishopContext _context;
@@ -18,10 +19,14 @@ namespace Products.Controllers
       _mapper = mapper;
     }
 
+///////////////////////////// GET //////////////////////////////
+
     [HttpGet]
     public async Task<ActionResult> GetProducts() 
     {
-      List<Product> products = await _context.Products.Include(po => po.ProductOrders).ToListAsync();
+      List<Product> products = await _context.Products
+        .Include(po => po.ProductOrders)
+        .Include(pc => pc.ProductCategories).ToListAsync();
 
       List<ProductDTO> productDTOs = _mapper.Map<List<ProductDTO>>(products);
 
@@ -41,6 +46,8 @@ namespace Products.Controllers
       return Ok(_mapper.Map<ProductDTO>(found));
     }
 
+//////////////////////////// POST //////////////////////////////
+
     [HttpPost]
     public async Task<ActionResult> CreateProduct(ProductDTO newProductDTO)
     {
@@ -51,7 +58,60 @@ namespace Products.Controllers
 
       return CreatedAtAction("CreateProduct", newProduct);
     }
+    
+//////////////////////////// PUT //////////////////////////////
+
+    [HttpPut("{id}")]
+      public async Task<IActionResult> PutProduct(int id, ProductDTO productDTO)
+    {
+      if (id != productDTO.Id)
+      {
+          return BadRequest();
+      }
+
+      _context.Entry(productDTO).State = EntityState.Modified;
+
+      try
+      {
+          await _context.SaveChangesAsync();
+      }
+      catch (DbUpdateConcurrencyException)
+      {
+          if (!ProductExists(id))
+          {
+              return NotFound();
+          }
+          else
+          {
+              throw;
+          }
+      }
+
+      return NoContent();
+    }
+
+//////////////////////////// DELETE //////////////////////////////
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteProduct(int id)
+    {
+        var product = await _context.Products.FindAsync(id);
+        if (product == null)
+        {
+            return NotFound();
+        }
+
+        _context.Products.Remove(product);
+        await _context.SaveChangesAsync();
+
+        return NoContent();
+    }
+  private bool ProductExists(int id)
+  {
+      return _context.Products.Any(e => e.Id == id);
   }
+  }
+
 }
 
 
